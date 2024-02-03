7cb38f79edb2968588303ea4cd4bd33fd395f28a
kuldeep <kuldeepyadav7291@gmail.com>
2024-02-03 18:46:01 +0530

    Public client entry [incomplete]

---
+++ auth-svc/src/main/resources/db/migrations/V20240201200115__public_client.sql
@@ -0,0 +1,23 @@
+insert into client(
+    client_id,
+    client_secret,
+    client_id_issued_at,
+    client_authentication_methods,
+    authorization_grant_types,
+    redirect_uris,
+    post_logout_redirect_uris,
+    scopes,
+    client_settings
+    ) values (
+        "public-web-client",
+        "{noop}secret",
+        now(),
+        "none",
+        "authorization_code",
+        "http://localhost:5173/oauth2/callback",
+        "openid,profile",
+        "{\"require-proof-key\":true,\"require-authorization-consent\":true}",
+        "{\"authorization-code-time-to-live\":5,\"access-token-time-to-live\":5,\"access-token-format\":\"self-contained\",\"device-code-time-to-live\":5,\"reuse-refresh-tokens\":true,\"refresh-token-time-to-live\":60,\"id-token-signature-algorithm\":\"RS256\"}"
+    );
+
+
